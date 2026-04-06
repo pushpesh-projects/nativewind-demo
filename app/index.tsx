c816845b129ae2d1e8a67fc73af04ebedf10e7b9
Pushpesh Kumar <pushpeshkumar26@gmail.com>
2026-04-06 10:53:08 +0530

    inset

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="relative h-80 bg-gray-200">
-      <View className="z-10 absolute bottom-10 left-10 bg-green-400 p-5">
-        <Text>Badge</Text>
-      </View>
-      <View className="bg-red-400 p-5">
-        <Text>Badge</Text>
+    <View className="relative h-40 bg-gray-200">
+      <View className="border-4 border-blue-800 absolute inset-0  justify-center items-center bg-black/50 ">
+        <Text className="text-white">Overlay</Text>
       </View>
     </View>
   );
