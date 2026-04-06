974c78e3ffb44bd70788f6d53e453cf5e96f1ccd
Pushpesh Kumar <pushpeshkumar26@gmail.com>
2026-04-06 11:44:19 +0530

    scroll view

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,31 @@
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text } from "react-native";
 
 export default function Index() {
   return (
-    <ScrollView className="flex-1 p-4">
-      <View className="space-y-4">
-        <Text className="bg-red-300 p-4">Item 1</Text>
-        <Text className="bg-blue-300 p-4">Item 2</Text>
-        <Text className="bg-green-300 p-4">Item 3</Text>
-        <Text className="bg-yellow-300 p-4">Item 4</Text>
-        <Text className="bg-purple-300 p-4">Item 5</Text>
-        <Text className="bg-red-300 p-4">Item 1</Text>
-        <Text className="bg-blue-300 p-4">Item 2</Text>
-        <Text className="bg-green-300 p-4">Item 3</Text>
-        <Text className="bg-yellow-300 p-4">Item 4</Text>
-        <Text className="bg-purple-300 p-4">Item 5</Text>
-        <Text className="bg-red-300 p-4">Item 1</Text>
-        <Text className="bg-blue-300 p-4">Item 2</Text>
-        <Text className="bg-green-300 p-4">Item 3</Text>
-        <Text className="bg-yellow-300 p-4">Item 4</Text>
-        <Text className="bg-purple-300 p-4">Item 5</Text>
-        <Text className="bg-red-300 p-4">Item 1</Text>
-        <Text className="bg-blue-300 p-4">Item 2</Text>
-        <Text className="bg-green-300 p-4">Item 3</Text>
-        <Text className="bg-yellow-300 p-4">Item 4</Text>
-        <Text className="bg-purple-300 p-4">Item 5</Text>
-        <Text className="bg-red-300 p-4">Item 1</Text>
-        <Text className="bg-blue-300 p-4">Item 2</Text>
-        <Text className="bg-green-300 p-4">Item 3</Text>
-        <Text className="bg-yellow-300 p-4">Item 4</Text>
-        <Text className="bg-purple-300 p-4">Item 5</Text>
-        <Text className="bg-red-300 p-4">Item 1</Text>
-        <Text className="bg-blue-300 p-4">Item 2</Text>
-        <Text className="bg-green-300 p-4">Item 3</Text>
-        <Text className="bg-yellow-300 p-4">Item 4</Text>
-        <Text className="bg-purple-300 p-4">Item 5</Text>
-      </View>
+    <ScrollView className="w-20 bg-gray-200  border border-black">
+      <Text className="text-xl">
+        This is a very long text that will overflow This is a very long text
+        that will overflow This is a very long text that will overflow This is a
+        very long text that will overflow This is a very long text that will
+        overflow This is a very long text that will overflow This is a very long
+        text that will overflow This is a very long text that will overflow This
+        is a very long text that will overflow This is a very long text that
+        will overflow This is a very long text that will overflow This is a very
+        long text that will overflow This is a very long text that will overflow
+        This is a very long text that will overflow This is a very long text
+        that will overflow This is a very long text that will overflow This is a
+        very long text that will overflow This is a very long text that will
+        overflow This is a very long text that will overflow This is a very long
+        text that will overflow This is a very long text that will overflow This
+        is a very long text that will overflow This is a very long text that
+        will overflow This is a very long text that will overflow This is a very
+        long text that will overflow This is a very long text that will overflow
+        This is a very long text that will overflow This is a very long text
+        that will overflow This is a very long text that will overflow This is a
+        very long text that will overflow This is a very long text that will
+        overflow This is a very long text that will overflow This is a very long
+        text that will overflow
+      </Text>
     </ScrollView>
   );
 }
