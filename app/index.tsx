765205ae053358e7a6758f7db629f258b97ac303
Pushpesh Kumar <pushpeshkumar26@gmail.com>
2026-04-06 11:53:31 +0530

    scrollable screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,16 @@
-import { ScrollView, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <ScrollView horizontal className="p-4">
-      <View className="flex-row gap-4">
-        <View className="w-40 h-40 bg-red-300" />
-        <View className="w-40 h-40 bg-blue-300" />
-        <View className="w-40 h-40 bg-green-300" />
-        <View className="w-40 h-40 bg-red-300" />
-        <View className="w-40 h-40 bg-blue-300" />
-        <View className="w-40 h-40 bg-green-300" />
-        <View className="w-40 h-40 bg-red-300" />
-        <View className="w-40 h-40 bg-blue-300" />
-        <View className="w-40 h-40 bg-green-300" />
-        <View className="w-40 h-40 bg-red-300" />
-        <View className="w-40 h-40 bg-blue-300" />
-        <View className="w-40 h-40 bg-green-300" />
+    <ScrollView className="flex-1 bg-gray-100 p-4">
+      <View className="space-y-4">
+        <View className="bg-white p-4 rounded-lg">
+          <Text>Card 1</Text>
+        </View>
+
+        <View className="bg-white p-4 rounded-lg">
+          <Text>Card 2</Text>
+        </View>
       </View>
     </ScrollView>
   );
