9abd35ea9918e97921296165230cc73b804a70ed
Pushpesh Kumar <pushpeshkumar26@gmail.com>
2026-04-06 11:46:19 +0530

    horizontal scrolling

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,22 @@
-import { ScrollView, Text } from "react-native";
+import { ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <ScrollView className="w-20 bg-gray-200  border border-black">
-      <Text className="text-xl">
-        This is a very long text that will overflow This is a very long text
-        that will overflow This is a very long text that will overflow This is a
-        very long text that will overflow This is a very long text that will
-        overflow This is a very long text that will overflow This is a very long
-        text that will overflow This is a very long text that will overflow This
-        is a very long text that will overflow This is a very long text that
-        will overflow This is a very long text that will overflow This is a very
-        long text that will overflow This is a very long text that will overflow
-        This is a very long text that will overflow This is a very long text
-        that will overflow This is a very long text that will overflow This is a
-        very long text that will overflow This is a very long text that will
-        overflow This is a very long text that will overflow This is a very long
-        text that will overflow This is a very long text that will overflow This
-        is a very long text that will overflow This is a very long text that
-        will overflow This is a very long text that will overflow This is a very
-        long text that will overflow This is a very long text that will overflow
-        This is a very long text that will overflow This is a very long text
-        that will overflow This is a very long text that will overflow This is a
-        very long text that will overflow This is a very long text that will
-        overflow This is a very long text that will overflow This is a very long
-        text that will overflow
-      </Text>
+    <ScrollView horizontal className="p-4">
+      <View className="flex-row gap-4">
+        <View className="w-40 h-40 bg-red-300" />
+        <View className="w-40 h-40 bg-blue-300" />
+        <View className="w-40 h-40 bg-green-300" />
+        <View className="w-40 h-40 bg-red-300" />
+        <View className="w-40 h-40 bg-blue-300" />
+        <View className="w-40 h-40 bg-green-300" />
+        <View className="w-40 h-40 bg-red-300" />
+        <View className="w-40 h-40 bg-blue-300" />
+        <View className="w-40 h-40 bg-green-300" />
+        <View className="w-40 h-40 bg-red-300" />
+        <View className="w-40 h-40 bg-blue-300" />
+        <View className="w-40 h-40 bg-green-300" />
+      </View>
     </ScrollView>
   );
 }
