3abd0ce7cdafa364277cd016edcb5e09cc78dfb7
Pushpesh Kumar <pushpeshkumar26@gmail.com>
2026-04-08 17:22:49 +0530

    using type for useRef

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Pressable, Text, TextInput, View } from "react-native";
 import ProfileCard from "./Profile";
 
 type User = {
@@ -8,6 +8,13 @@ type User = {
 };
 export default function Home() {
   const [user, setUser] = useState<User | null>(null);
+  const inputRef = useRef<TextInput | null>(null);
+
+  useEffect(() => {
+    if (user) {
+      inputRef.current?.focus();
+    }
+  }, [user]);
 
   if (!user)
     return (
@@ -15,10 +22,13 @@ export default function Home() {
         <View className="bg-gray-200 rounded-lg mb-5">
           <Text className="p-10 text-center">No user</Text>
         </View>
+
         <View>
           <Pressable
             className="bg-blue-400"
-            onPress={() => setUser({ name: "Pushpesh", age: 35 })}
+            onPress={() => {
+              setUser({ name: "Pushpesh", age: 35 });
+            }}
           >
             <Text className="p-5  text-black text-center font-bold">
               {" "}
@@ -31,6 +41,11 @@ export default function Home() {
 
   return (
     <View className=" rounded-lg m-20">
+      <TextInput
+        ref={inputRef}
+        className="p-5 border border-gray-500 mb-5 rounded-md"
+        placeholder="Enter your name"
+      ></TextInput>
       <ProfileCard name={user.name} age={user.age}>
         <Text>Frontend Developer</Text>
       </ProfileCard>
