7ac2a3d1ca9ed7b2a12d104690d43ec5f8637373
Pushpesh Kumar <pushpeshkumar26@gmail.com>
2026-04-08 16:32:03 +0530

    using types with useState hook

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,42 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
 import ProfileCard from "./Profile";
 
+type User = {
+  name: string;
+  age: number;
+};
 export default function Home() {
+  const [user, setUser] = useState<User | null>(null);
+
+  if (!user)
+    return (
+      <View className="m-20 ">
+        <View className="bg-gray-200 rounded-lg mb-5">
+          <Text className="p-10 text-center">No user</Text>
+        </View>
+        <View>
+          <Pressable
+            className="bg-blue-400"
+            onPress={() => setUser({ name: "Pushpesh", age: 35 })}
+          >
+            <Text className="p-5  text-black text-center font-bold">
+              {" "}
+              Load User
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    );
+
   return (
-    <View className="bg-gray-200 rounded-lg m-20">
-      <ProfileCard name="Pushpesh" age={30}>
+    <View className=" rounded-lg m-20">
+      <ProfileCard name={user.name} age={user.age}>
         <Text>Frontend Developer</Text>
       </ProfileCard>
+      <Pressable className="bg-blue-400 mt-5" onPress={() => setUser(null)}>
+        <Text className="text-center  text-black  p-5">Remove User</Text>
+      </Pressable>
     </View>
   );
 }
